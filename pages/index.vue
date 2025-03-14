2bb6c6136a897250437387eff9cb011944a1015a
rizqya-adzra <rizqyaadzrazahirasudrajat@smkwikrama.sch.id>
2025-03-14 14:54:20 +0700

    nambah login register

--- pages/index.vue
+++ pages/index.vue
@@ -9,46 +9,38 @@
 
       
       <!-- Hamburger Button -->
+       <div class="flex items-center gap-8">
+         <label class="relative flex items-center cursor-pointer ">
+           <input type="checkbox" class="sr-only peer" v-model="isDark" />
+           <div
+           class="w-14 h-7 bg-gray-300 rounded-full peer-checked:bg-blue-600 relative transition-colors duration-300 flex items-center px-1"
+           >
+          <div
+          class="w-5 h-5 bg-white rounded-full transition-transform duration-300 ease-in-out flex items-center justify-center shadow-md"
+            :class="isDark ? 'translate-x-7' : 'translate-x-0'"
+            >
+            <svg v-if="!isDark" class="w-4 h-4 text-yellow-500" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+            </svg>
+            <svg v-if="isDark" class="w-4 h-4 text-yellow-500" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
+            </svg>
+          </div>
+        </div>
+      </label>
       <Menu @click="toggleSlideMenu" class="cursor-pointer md:hidden dark:text-white" />
       
       <!-- Menu Items -->
       <div class="hidden md:flex gap-3 md:gap-5 items-center">
-        <label class="relative flex items-center cursor-pointer ">
-            <input type="checkbox" class="sr-only peer" v-model="isDark" />
-            <div
-              class="w-14 h-7 bg-gray-300 rounded-full peer-checked:bg-blue-600 relative transition-colors duration-300 flex items-center px-1"
-            >
-              <div
-                class="w-5 h-5 bg-white rounded-full transition-transform duration-300 ease-in-out flex items-center justify-center shadow-md"
-                :class="isDark ? 'translate-x-7' : 'translate-x-0'"
-              >
-                <svg v-if="!isDark" class="w-4 h-4 text-yellow-500" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
-                </svg>
-                <svg v-if="isDark" class="w-4 h-4 text-yellow-500" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
-                </svg>
-              </div>
-            </div>
-          </label>
         <BaseButtonPrimaryButton :onClick="() => navigateTo('/login')" buttonName="Sign In" class="px-4 md:px-5" />
-        <BaseButtonTertiaryButton :onClick="() => navigateTo('/register')" buttonName="Sign Up" class="p-2 md:p-3" />
-          
+          <BaseButtonTertiaryButton :onClick="() => navigateTo('/register')" buttonName="Sign Up" class="p-2 md:p-3" />
+            
+          </div>
+        </div>
       </div>
-    </div>
   </nav>
   <div v-if="showSlideMenu" class="fixed inset-0 bg-black/50 z-20" @click="toggleSlideMenu"></div>
 
-<<<<<<< HEAD
-  <transition name="slide">
-  <div v-if="showSlideMenu" class="fixed inset-0 bg-black/50 z-20" @click="toggleSlideMenu">
-    <div class="bg-white dark:bg-gray-800 w-64 h-full p-5 shadow-lg relative right-0" @click.stop>
-      <button @click="toggleSlideMenu" class="absolute top-5 right-5 text-xl">✖</button>
-      <div class="mt-10 flex flex-col gap-4">
-        <BaseButtonPrimaryButton :onClick="() => navigateTo('/login')" buttonName="Sign In" class="px-4 py-2" />
-        <BaseButtonTertiaryButton :onClick="() => navigateTo('/register')" buttonName="Sign Up" class="px-4 py-2" />
-          
-=======
     <transition name="slide">
     <div v-if="showSlideMenu" class="fixed inset-0 z-20" @click="toggleSlideMenu">
       <div class="flex flex-col">
@@ -59,10 +51,8 @@
             <BaseButtonTertiaryButton :onClick="() => navigateTo('/register')" buttonName="Sign Up" class="px-4 py-2" />
           </div>
         </div>
->>>>>>> 4ba9d26c87e895d318fc601c46fc75730fef94e4
       </div>
     </div>
-  </div>
 </transition>
 
   <!-- Hero Section -->
@@ -234,10 +224,6 @@ definePageMeta({
 
 .slide-enter-from,
 .slide-leave-to {
-<<<<<<< HEAD
-  transform: translateX(100%);
-=======
   transform: translateY(-100%);
->>>>>>> 4ba9d26c87e895d318fc601c46fc75730fef94e4
 }
 </style>
